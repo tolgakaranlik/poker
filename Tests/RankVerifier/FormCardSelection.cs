13bd7f397fbd014e9f64aa76748e055f81cf43c3
tolgakaranlik@gmail.com <47294027ttT>
2020-06-25 00:31:27 +0300

    Test projesinin eklenmesi

---
+++ Tests/RankVerifier/FormCardSelection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RankVerifier
+{
+    public partial class FormCardSelection : Form
+    {
+        public string Result = "N/A";
+
+        public FormCardSelection()
+        {
+            InitializeComponent();
+        }
+
+        private void pbS1C1_Click(object sender, EventArgs e)
+        {
+            Result = (sender as PictureBox).Name;
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
